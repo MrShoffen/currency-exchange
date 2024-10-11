7b5e1e4485e263e1e30acbe36faecbd33b1872e6
mrshoffen <mrshoffen@gmail.com>
2024-10-11 17:31:40 +0300

    chore: add dependency to hibernate validator and EL lbrary

--- src/main/java/org/mrshoffen/exchange/dto/request/CurrencyRequestDto.java
+++ src/main/java/org/mrshoffen/exchange/dto/request/CurrencyRequestDto.java
@@ -1,12 +1,40 @@
 package org.mrshoffen.exchange.dto.request;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Value;
+import org.hibernate.validator.HibernateValidator;
+
+import java.util.Set;
 
 @Value
 @Builder
 public class CurrencyRequestDto {
+
+    @NotBlank
     String code;
+
+
     String name;
+
+
     String sign;
+
+    public static void main(String[] args) {
+
+        Validator validator = Validation.byProvider(HibernateValidator.class)
+                .configure()
+                .buildValidatorFactory().getValidator();
+
+        CurrencyRequestDto hfd = CurrencyRequestDto.builder()
+                .code("   ")
+                .build();
+
+        Set<ConstraintViolation<CurrencyRequestDto>> validate = validator.validate(hfd);
+
+        System.out.println(validate.size());
+    }
 }
